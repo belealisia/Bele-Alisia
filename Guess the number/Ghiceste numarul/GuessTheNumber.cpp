b838e11009ca88dcec0823d03fe2b672547c42d0
belealisia <iasminabele@gmail.com>
2026-04-16 10:37:36 +0200

    hangman

--- Guess the number/Ghiceste numarul/GuessTheNumber.cpp
+++ Guess the number/Ghiceste numarul/GuessTheNumber.cpp
@@ -10,15 +10,26 @@ int main()
 {
     srand(time(0));
     int randomNumber=rand()%101,attempt=10,attemptUsed=0,guess;
-    
-    while(attempt!=0)
+
+
+
+    cout<<"Instruction!!!"<<endl;
+    cout<<"Your calculator will generate a random number, and you will have to guess it."<<endl;
+    cout<<"You have 10 tries, for each guess the game will tell you if the number is higher, lower or if you guessed it."<<endl;
+    cout<<"Also if you don't enter the number, the game will show you on the screen ...You lost! The number was..."<<endl;
+    cout<<"Very lucky! Start!"<<endl;
+
+
+
+    while(attempt!=0&&guess!=randomNumber)
     {
+        cout<<"Enter a number ";
         cin>>guess;
         attempt--;
         attemptUsed++;
         if(guess==randomNumber)
             {
-                cout<<"Congratulations! The number is "<<guess<<endl;
+                cout<<"Congratulations! The number is "<<guess<<"."<<endl;
                 cout<<"You uesd "<<attemptUsed<<" attempt."<<endl;
             }
         else 
